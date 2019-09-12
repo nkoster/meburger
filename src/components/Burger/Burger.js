454cc5aeaf4481b67f5cc70563f7b579dbdf14b6
Niels Koster <n.koster@portavita.eu>
2019-09-13 00:03:24 +0200

    Initial

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Burger extends Component {
+    render() {
+        return (
+            <p>BURGER</p>
+        )
+    }
+}
+
+export default Burger
