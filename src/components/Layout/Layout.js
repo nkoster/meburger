2393ee250c4ec2aa8d58b24cff368f30368845b3
Niels Koster <n.koster@portavita.eu>
2019-09-12 23:28:58 +0200

    Basic Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Aux from '../../hoc/Aux'
 
-const Layout = () => (
-    <div></div>
-)
+const Layout = props =>
+    <Aux>
+        <div>Toolbar SideDrawer Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
 
 export default Layout
