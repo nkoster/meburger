47685e342427eb739a76bfd1ab9b148b171bad8e
Niels Koster <n.koster@portavita.eu>
2019-10-22 22:58:27 +0200

    Use BuildControl component

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
 
 const BuildControls = () => (
-    <div className={classes.BuildControls}>aap</div>
+    <div className={classes.BuildControls}>
+        <BuildControl label='blaaa'/>
+    </div>
 )
 
 export default BuildControls
