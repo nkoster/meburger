4213a07bc0dc29964688f022cfef1689dea6dcb8
Niels Koster <n.koster@portavita.eu>
2019-10-22 22:03:18 +0200

    Reduce ingredients array to be able to count + show placeholder when zero

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = props => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingrKey => {
             return [...Array(props.ingredients[ingrKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={ingrKey + i} type={ingrKey} />
                 })
         })
+        .reduce((arr, element) => {
+            return arr.concat(element)
+        }, [])
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Fill Me!</p>
+    }
+    console.log(transformedIngredients)
 
     return (
         <div className={classes.Burger}>
