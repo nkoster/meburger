afde51f6ea23b07683b7b40ae5daa9b16f488195
Niels Koster <n.koster@portavita.eu>
2019-10-23 00:12:16 +0200

    Use added() on click

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ const BuildControl = props => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>less</button>
-        <button className={classes.More}>more</button>
+        <button className={classes.More} onClick={props.added}>more</button>
     </div>
 )
 
