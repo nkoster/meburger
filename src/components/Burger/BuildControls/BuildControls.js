20892fa9dfb8e718d6ee0584aab5eda6e283706e
Niels Koster <n.koster@portavita.eu>
2019-10-22 23:10:27 +0200

    Show BuildControl components from an array

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,9 +2,18 @@ import React from 'react'
 import classes from './BuildControls.module.css'
 import BuildControl from './BuildControl/BuildControl'
 
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
+
 const BuildControls = () => (
     <div className={classes.BuildControls}>
-        <BuildControl label='blaaa'/>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
 )
 
