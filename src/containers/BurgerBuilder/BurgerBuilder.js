152a369270c38a5817d78d400d5893bd901097f0
Niels Koster <n.koster@portavita.eu>
2019-10-23 00:32:53 +0200

    Add remove method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,7 +39,20 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = type => {
-        
+        const oldCount = this.state.ingredients[type]
+        if (oldCount === 0) return
+        const newCount = oldCount - 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = newCount
+        const priceDeduction = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceDeduction
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })        
     }
 
     render() {
@@ -47,7 +60,8 @@ class BurgerBuilder extends Component {
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler} />
             </Aux>
         )
     }
