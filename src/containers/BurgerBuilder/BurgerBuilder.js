2b5ca9b98046df82946db928b903dd7f6b6daf70
Niels Koster <n.koster@portavita.eu>
2019-10-23 00:14:34 +0200

    Add price list and addIngredientHandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    salad: 0.50,
+    bacon: 1.00,
+    cheese: 1.00,
+    meat: 1.50
+}
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,14 +18,36 @@ class BurgerBuilder extends Component {
             cheese: 0,
             bacon: 0,
             meat: 1
+        },
+        totalPrice: 2
+    }
+
+    addIngredientHandler = type => {
+        const oldCount = this.state.ingredients[type]
+        const newCount = oldCount + 1
+        const updatedIngredients = {
+            ...this.state.ingredients
         }
+        updatedIngredients[type] = newCount
+        const priceAddition = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
+    }
+
+    removeIngredientHandler = type => {
+        
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} />
             </Aux>
         )
     }
