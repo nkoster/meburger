444e708e9da7f03a09a9a155d8144ea673054499
Niels Koster <n.koster@portavita.eu>
2019-10-22 22:24:26 +0200

    Initial, burger build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './BuildControls.module.css'
+
+const BuildControls = () => (
+    <div className={classes.BuildControls}>aap</div>
+)
+
+export default BuildControls
