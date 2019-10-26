ecbd0c722dd9c44663617156473b9fe70be49416
Niels Koster <n.koster@portavita.eu>
2019-10-26 15:33:36 +0200

    Use Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
+import Toolbar from '../Navigator/Toolbar/Toolbar'
+
 import classes from './Layout.module.css'
 
 const Layout = props =>
     <Aux>
-        <div>Toolbar SideDrawer Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
