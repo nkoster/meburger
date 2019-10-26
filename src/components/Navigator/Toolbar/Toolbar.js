824687681fc02a6e16a9263f286aa63324fe9fca
Niels Koster <n.koster@portavita.eu>
2019-10-26 15:33:08 +0200

    Initial, Toolbar component

---
+++ src/components/Navigator/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Toolbar.module.css'
+
+const Toolbar = props => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            <ul>...</ul>
+        </nav>
+    </header>
+)
+
+export default Toolbar
